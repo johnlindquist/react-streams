ad71d7f1c0a3857cebe326a87b7455a5d09a038b
johnlindquist <johnlindquist@gmail.com>
2018-04-09 07:49:57 -0600

    deprecating swtichProps and streamContext, need more thought

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { createElement, Component, createContext } from "react"
+import { Component } from "react"
 
 import { Subject } from "rxjs"
-import { startWith, switchMap, distinctUntilChanged } from "rxjs/operators"
+import { startWith, distinctUntilChanged } from "rxjs/operators"
 
 const pipeProps = (...operations) => {
   return class extends Component {
     setState$ = new Subject()
     subscription
-    state = {}
 
     __renderFn = this.props.children
       ? this.props.children
@@ -32,55 +31,6 @@ const pipeProps = (...operations) => {
     }
   }
 }
-/*
-Covering cases:
-(input$, "text") //switch to stream, startWith second param
-(ajax("http://")) //switch to stream, no startWith
-(ajax, ({url}) => url) //switch to creation fn, create with url from props
-*/
-const switchProps = (observableOrFn, optionalSelectOrValue) => (
-  ...operations
-) =>
-  pipeProps(
-    switchMap(props => {
-      const optionalValue =
-        optionalSelectOrValue instanceof Function
-          ? optionalSelectOrValue(props)
-          : optionalSelectOrValue
-
-      const observable =
-        optionalValue !== undefined
-          ? observableOrFn instanceof Function
-            ? observableOrFn(optionalValue)
-            : observableOrFn.pipe(startWith(optionalValue))
-          : observableOrFn
-      return observable
-    }),
-    ...operations
-  )
-
-const streamProviderConsumer = stream$ => {
-  const { Provider, Consumer } = createContext()
-
-  class StreamProvider extends Component {
-    subscription
-    componentDidMount() {
-      this.subscription = stream$.subscribe(this.setState.bind(this))
-    }
-
-    render() {
-      return this.state
-        ? createElement(Provider, { value: this.state }, this.props.children)
-        : null
-    }
-
-    componentWillUnmount() {
-      this.subscription.unsubscribe()
-    }
-  }
-
-  return [StreamProvider, Consumer]
-}
 
 const sourceNext = (...args) => {
   const subject = new Subject()
@@ -88,4 +38,4 @@ const sourceNext = (...args) => {
   return [subject.pipe(...args), subject.next.bind(subject)]
 }
 
-export { pipeProps, switchProps, streamProviderConsumer, sourceNext }
+export { pipeProps, sourceNext }
