7e11460b0c795a7d91532625e4871ffb30777a21
johnlindquist <johnlindquist@gmail.com>
2018-06-11 14:09:31 -0600

    refactor to allow an observable to return a fn

--- src/operators/patchScan.ts
+++ src/operators/patchScan.ts
@@ -1,14 +1,21 @@
 import { isObservable, of, pipe } from "rxjs"
-import { scan, switchMap } from "rxjs/operators"
+import { mergeScan, switchMap } from "rxjs/operators"
 
 export const patchScan: any = pipe(
-  scan((state, update: any) => {
-    if (update instanceof Function) return update(state)
-    return { ...state, ...update }
-  }),
-  switchMap((result: any) => {
-    if (isObservable(result)) return result
+  (mergeScan as any)((state, update) => {
+    if (update instanceof Function) {
+      const result = update(state)
+      if (isObservable(result)) {
+        return result.pipe(
+          switchMap(foo => {
+            if (foo instanceof Function) return of(foo(state))
+            return of(foo)
+          })
+        )
+      }
 
-    return of(result)
+      return of(result)
+    }
+    return of({ ...state, ...update })
   })
 )
