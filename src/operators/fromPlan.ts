7a90a2785556a5a00f4459c8f570c116b1fa3f21
johnlindquist <johnlindquist@gmail.com>
2018-06-13 17:36:01 -0600

    removing fromPlan and toPlan

--- src/operators/fromPlan.ts
+++
@@ -1,8 +0,0 @@
-import { pipe, UnaryFunction } from "rxjs"
-import { map, withLatestFrom } from "rxjs/operators"
-
-export const fromPlan = (otherPlan, selector): UnaryFunction<any, any> =>
-  pipe(
-    withLatestFrom(otherPlan, (_, value) => value),
-    map(selector)
-  )
--- src/operators/toPlan.ts
+++
@@ -1,9 +0,0 @@
-import { merge, of, pipe, UnaryFunction } from "rxjs"
-import { first, map, switchMap, tap } from "rxjs/operators"
-
-export const toPlan = (otherPlan, selector = x => x): UnaryFunction<any, any> =>
-  pipe(
-    tap(value => console.log(`toPlan`, value)),
-    map(selector),
-    switchMap(value => merge(of(value), otherPlan(value).pipe(first())))
-  )
