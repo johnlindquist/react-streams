e6e2fa5cc97edd42c6fae0fc6da705abb951e748
johnlindquist <johnlindquist@gmail.com>
2018-06-13 17:44:44 -0600

    update alias in config

--- examples/poi.config.js
+++ examples/poi.config.js
@@ -18,7 +18,7 @@ module.exports = {
   configureWebpack(config, context) {
     debugger
     config.resolve.alias = {
-      "react-streams": path.resolve(__dirname, "../dist/")
+      "react-streams": path.resolve(__dirname, "../dist/react-streams.js")
     }
 
     config.module.rules.push({
